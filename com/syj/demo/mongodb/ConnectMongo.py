9e6a9276e61e9b90050147665020af53ce115467
syj <495054021@qq.com>
2019-07-14 08:37:09 +0800

    安装mongodb mysql依赖,使用Python创建链接进行交互

--- com/syj/demo/mongodb/ConnectMongo.py
+++ com/syj/demo/mongodb/ConnectMongo.py
@@ -6,5 +6,21 @@ import pymongo
 client = pymongo.MongoClient('localhost',27017)
 # 连接到指定数据库spider_mongodb
 db = client.spider_mongodb
-# 获取表page_table数据
+# 获取表page_table这个集合
 collection = db.page_table
+print '--------->获取表集合成功'
+# 插入文档,注意此时要先启mongol服务,否则会报拒绝链接
+books = [{"author": "Mike",
+          "text": "My first book!",
+          "tags": ["爬虫", "python", "网络"],
+          "date": "2019-07-13"
+          },
+         {"author": "qiye",
+          "text": "My sec book!",
+          "tags": ["hack", "python", "渗透"],
+          "date": "2019-07-14"
+         }]
+collection.insert(books)
+print '-------->写入数据成功'
+for book in collection.find():
+    print book
--- com/syj/demo/mysql/ConnectMysql.py
+++ com/syj/demo/mysql/ConnectMysql.py
@@ -2,8 +2,19 @@
 
 import MySQLdb
 
-con = MySQLdb.connect(host='localhost', user='root', passwd='syjmysql2015', db='spider_db', port=3306,
-                      charset='utf-8')
+con = MySQLdb.connect(host='localhost', user='root', passwd='syjmysql2015', db='spider_db',
+                      port=3306, charset='utf8')
 cur = con.cursor()
-cur.execute('create table person(id int not null)')
-print 'ok'
+'''
+cur.execute('create table person_info(id int not null auto_increment primary key, '
+            'name varchar(60),'
+            'age int)')
+'''
+cur.executemany('insert into person_info(name,age) values(%s, %s)', [('syj', 18), ('jack', 20)])
+con.commit()
+print '--------->数据写入成功ok'
+
+cur.execute('select * from person_info')
+res = cur.fetchall()
+for line in res:
+    print line
