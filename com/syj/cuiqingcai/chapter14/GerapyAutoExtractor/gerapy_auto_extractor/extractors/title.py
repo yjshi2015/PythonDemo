79bd0620b876ad598ac2729861c08d43dc69f54c
syj <495054021@qq.com>
2022-06-29 07:54:34 +0800

    完成title提取逻辑

--- com/syj/cuiqingcai/chapter14/GerapyAutoExtractor/gerapy_auto_extractor/extractors/title.py
+++ com/syj/cuiqingcai/chapter14/GerapyAutoExtractor/gerapy_auto_extractor/extractors/title.py
@@ -1,8 +1,8 @@
-from .base import BaseExtractor
+from com.syj.cuiqingcai.chapter14.GerapyAutoExtractor.gerapy_auto_extractor.extractors.base import BaseExtractor
 from lxml.html import HtmlElement
-from ..patterns.title import METAS
-from ..utils.lcs import lcs_of_2
-from ..utils.similarity import similarity2
+from com.syj.cuiqingcai.chapter14.GerapyAutoExtractor.gerapy_auto_extractor.patterns.title import METAS
+from com.syj.cuiqingcai.chapter14.GerapyAutoExtractor.gerapy_auto_extractor.utils.lcs import lcs_of_2
+from com.syj.cuiqingcai.chapter14.GerapyAutoExtractor.gerapy_auto_extractor.utils.similarity import similarity2
 
 
 class TitleExtractor(BaseExtractor):
@@ -13,14 +13,53 @@ class TitleExtractor(BaseExtractor):
             if title:
                 return ''.join(title)
 
-
     def extract_by_title(self, element: HtmlElement) -> str:
         return ''.join(element.xpath('//title//text()')).strip()
 
-
-    def extract_by_h2(self, element: HtmlElement) -> str:
+    def extract_by_hs(self, element: HtmlElement) -> str:
         hs = element.xpath('.//h1//text()|//h2//text()|//h3//text()')
         return hs or []
 
-
     def extract_by_h(self, element: HtmlElement) -> str:
+        for xpath in ['//h1', '//h2', '//h3']:
+            children = element.xpath(xpath)
+            if not children:
+                continue
+            child = children[0]
+            texts = child.xpath('./text()')
+            if texts and len(texts):
+                return texts[0].strip()
+
+    def process(self, element: HtmlElement):
+        title_extracted_by_metas = self.extract_by_meta(element)
+        title_extracted_by_h = self.extract_by_h(element)
+        title_extracted_by_hs = self.extract_by_hs(element)
+        title_extracted_by_title = self.extract_by_title(element)
+
+        if title_extracted_by_metas:
+            return title_extracted_by_metas
+
+        title_extracted_by_hs = sorted(title_extracted_by_hs,
+                                       key=lambda x: similarity2(x, title_extracted_by_title),
+                                       reverse=True)
+        if title_extracted_by_hs:
+            return lcs_of_2(title_extracted_by_hs[0], title_extracted_by_title)
+
+        if title_extracted_by_title:
+            return title_extracted_by_title
+
+        return title_extracted_by_h
+
+
+title_extractor = TitleExtractor()
+
+
+def extract_title(html):
+    title = title_extractor.extract(html)
+    return title
+
+
+if __name__ == '__main__':
+    with open('../detail.html', encoding='utf-8') as f:
+        title = extract_title(f.read())
+    print(title)
